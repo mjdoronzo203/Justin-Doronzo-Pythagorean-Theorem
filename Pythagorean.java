db92caeebd46fe8c65867ceb020d6708b55fe13d
mjdoronzo203 <50427888+mjdoronzo203@users.noreply.github.com>
2019-06-17 10:37:43 -0500

    Add files via upload

---
+++ Pythagorean.java
@@ -0,0 +1,6 @@
+public class Pythagorean {
+	public double calculateHypotenuse(int legA, int legB) {
+		double hypotenuse = Math.sqrt(legA) + Math.sqrt(legB);
+		return hypotenuse;
+	}
+}
---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+	public static void main(String[] args) {
+		Pythagorean iD = new Pythagorean();
+		double c = iD.calculateHypotenuse(25, 9);
+		System.out.println("The hypotenuse is " + c);
+	}
+}
